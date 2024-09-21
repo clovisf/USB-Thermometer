fac9c152347310cd78de4627bbc1637b15feee48
Clovis Fritzen <clovisf@gmail.com>
2024-09-21 19:02:28 -0300

    added support to thingspeak
    added support to thingspeak

--- HTU21D-temp-humidity-light/HTU21D-temp-humidity-light.ino
+++ HTU21D-temp-humidity-light/HTU21D-temp-humidity-light.ino
@@ -16,6 +16,17 @@
 long oldtime1;
 long oldtime2;
 
+#include <WiFi.h>
+#include "ThingSpeak.h"
+
+const char* ssid = "Clovis 2.4G";   // your network SSID (name) 
+const char* password = "99143304";   // your network password
+
+WiFiClient  client;
+
+unsigned long myChannelNumber = 2515409;
+const char * myWriteAPIKey = "Z3YRH78K0AFE6HLU";
+
 // Connect Vin to 3-5VDC
 // Connect GND to ground
 // Connect SCL to I2C clock pin (A5 on UNO)
@@ -32,11 +43,26 @@ void setup() {
     Serial.println("Couldn't find sensor!");
     while (1);
   }
+
+  WiFi.mode(WIFI_STA);   
+  
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+
 }
 
 void loop() {
 
-    if(micros() - oldtime1 > 990000){
+    if(micros() - oldtime1 > 59990000){
+
+      if(WiFi.status() != WL_CONNECTED){
+        Serial.print("Attempting to connect");
+        while(WiFi.status() != WL_CONNECTED){
+          WiFi.begin(ssid, password); 
+          delay(5000);     
+        } 
+        Serial.println("\nConnected.");
+      }
+
       oldtime1= micros();
       float temp = htu.readTemperature();
       float rel_hum = htu.readHumidity();
@@ -46,8 +72,13 @@ void loop() {
       Serial.print("Humidity: "); Serial.print(rel_hum); Serial.println(" \%");
       Serial.print("Light[0-3300]: ");
       Serial.println(analogVolts);
+      ThingSpeak.setField(1, temp);
+      ThingSpeak.setField(2, rel_hum);
+      ThingSpeak.setField(3, analogVolts);
+      int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+      
     }
-    if(micros() - oldtime2 > 290000){
+    if(micros() - oldtime2 > 190000){
       oldtime2= micros();
       digitalWrite(ledPin, !digitalRead(ledPin));
     }
