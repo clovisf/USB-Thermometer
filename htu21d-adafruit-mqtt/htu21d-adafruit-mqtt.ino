9a93f2e6ae36c58d0733c5eb77c7972a664369d1
Clovis Fritzen <clovisf@gmail.com>
2024-10-29 14:44:46 -0300

    Update htu21d-adafruit-mqtt.ino
    removed passwords and keys

--- htu21d-adafruit-mqtt/htu21d-adafruit-mqtt.ino
+++ htu21d-adafruit-mqtt/htu21d-adafruit-mqtt.ino
@@ -23,8 +23,8 @@
 
 /************************* WiFi Access Point *********************************/
 
-#define WLAN_SSID "Clovis 2.4G"
-#define WLAN_PASS "99143304"
+#define WLAN_SSID ""
+#define WLAN_PASS ""
 
 /************************* Adafruit.io Setup *********************************/
 
@@ -35,8 +35,8 @@
 
 // Adafruit IO Account Configuration
 // (to obtain these values, visit https://io.adafruit.com and click on Active Key)
-#define AIO_USERNAME "clovisf"
-#define AIO_KEY      "b7a273ff14c04eb799f525ab07316d94"
+#define AIO_USERNAME ""
+#define AIO_KEY      ""
 
 /************ Global State (you don't need to change this!) ******************/
 
