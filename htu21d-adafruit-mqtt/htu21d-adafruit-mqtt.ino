2479fa2d0e3eb094fef0391eea5e7248e756aab3
Clovis Fritzen <clovisf@gmail.com>
2025-02-16 09:25:20 -0300

    edited code

--- htu21d-adafruit-mqtt/htu21d-adafruit-mqtt.ino
+++ htu21d-adafruit-mqtt/htu21d-adafruit-mqtt.ino
@@ -178,17 +178,18 @@ void MQTT_connect() {
 
   Serial.print("Connecting to MQTT... ");
 
-  uint8_t retries = 3;
+  uint8_t retries = 300;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
        Serial.println(mqtt.connectErrorString(ret));
        Serial.println("Retrying MQTT connection in 5 seconds...");
        mqtt.disconnect();
        delay(5000);  // wait 5 seconds
+       MQTT_connect();
        retries--;
-       if (retries == 0) {
+       /*if (retries == 0) {
          // basically die and wait for WDT to reset me
          while (1);
-       }
+       }*/
   }
 
   Serial.println("MQTT Connected!");
